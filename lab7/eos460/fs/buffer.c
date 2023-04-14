5730e102c601d32a234ad0034ea7e4a491d515d1
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2023-04-13 22:53:53 -0700

    standard algo

--- lab7/eos460/fs/buffer.c
+++ lab7/eos460/fs/buffer.c
@@ -4,6 +4,7 @@
 struct buf buffer[NBUF];
 struct buf *freelist;
 struct semaphore freebuf;
+int freelist_wanted = 0; // free list wanted flag
 
 struct devtab devtab[NDEV];
 
@@ -17,42 +18,125 @@ int binit()
   writebuf.buf = 0x700000 + 1024;
 }
 
+/* getblk: return a buffer=(dev,blk) for exclusive use */
 struct buf *getblk(int dev, int blk)
 {
-  // just need a buffer, so let it be the writebuf
-  struct buf *bp = &writebuf;
-  bp->dev = dev; bp->blk = blk;
-  bp->dirty = 0;
-  return bp;
+  struct buf *bp;
+  while (1)
+  {
+    // (1). search dev_list for a bp=(dev, blk)
+    bp = devtab[dev].dev_list;
+    // (2). if bp in dev_list
+    while (bp)
+    {
+      if (bp->dev == dev && bp->blk == blk)
+      {
+        if (bp->busy == 1)
+        {
+          bp->wanted = 1;
+          ksleep(bp); // wait for bp to be released
+          continue;
+        }
+        else
+        {
+          // bp not busy
+          bp = freelist;
+          struct buf *prev;
+          while (bp)
+          {
+            if (bp->dev == dev && bp->blk == blk)
+            {
+              // remove bp from free list
+              if (bp == freelist)
+              {
+                freelist = bp->next_free;
+              }
+              else
+              {
+                prev->next_free = bp->next_free;
+              }
+              break;
+            }
+            prev = bp;
+            bp = bp->next_free;
+          }
+
+          bp->busy = 1;
+          return bp;
+        }
+      }
+      bp = bp->next_dev;
+    }
+    // (3). bp not in cache, try to get a free buf from freelist
+    if (!freelist)
+    {
+      freelist_wanted = 1;
+      ksleep(freelist); // wait for any free buffer
+      continue; // retry the algorithm
+    }
+    // (4). freelist not empty
+    else
+    {
+      bp = freelist;
+      if (bp)
+        freelist = bp->next_free;
+      bp->busy = 1;
+      if (bp->dirty) // bp is for deplayed write
+      {
+        awrite(bp); // write bp out ASYNC
+        continue;
+      }
+      // (5). reassign bp to (dev,blk); set bp data invalid, etc
+      bp->dev = dev;
+      bp->blk = blk;
+      bp->valid = 0;
+    }
+  }
 } 
+
 
 struct buf *bread(int dev, int blk)
 {
-  // use readbuf to read in a block; return readbuf pointer
-  //printf("bread %d %d ", dev, blk);
-  
-  struct buf *bp = &readbuf;
-  bp->dev = dev; bp->blk = blk;
-  getblock(blk, bp->buf);
-  //printf("end bread\n");
+  struct buf *bp = getblk(dev, blk); // get a buffer for (dev,blk)
+  if (bp->valid == 0) {
+    bp->opcode = 0x18; // READ
+    getblock(bp->blk, bp->buf);
+    bp->valid = 1;
+  }
   return bp;
 }
 
-int bwrite(struct buf *bp)
+int bwrite(struct buf *bp) // release bp marked VALID but DIRTY
 { 
-  // write out bp->buf to bp->blk
-  putblock(bp->blk, bp->buf);
+  bp->valid = 1;
+  bp->dirty = 1;
+  brelse(bp);
 }
 
-int awrite(struct buf *bp)
+int awrite(struct buf *bp) // write DIRTY bp out to disk
 {
-  // no async write; all writes are immediate
-  bwrite(bp);
+  bp->opcode = 0x25; // WRITE
+  putblock(bp->blk, bp->buf);
+  bp->dirty = 0; // turn off DIRTY flag
+  brelse(bp); // bp VALID and in buffer cache
 }
 
+/* brelse: releases a buffer as FREE to freelist */
 int brelse(struct buf *bp)
 {
-  // no need to release any buffer
+  if (bp->wanted)
+    kwakeup(bp); // wakeup ALL proc's sleeping on bp
+  if (freelist_wanted)
+    kwakeup(freelist); // wakeup ALL proc's sleeping on bp
+
+  // clear free list wanted flags
+  bp->wanted = 0;
+  freelist_wanted = 0;
+  
+  struct buf *tmp = freelist;
+  while (tmp->next_free)
+    tmp = tmp->next_free;
+  tmp->next_free = bp;
 }
 
 int khits(int y, int z) // syscall hits entry
