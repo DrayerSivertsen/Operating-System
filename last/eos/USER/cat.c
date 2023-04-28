6c9ea04c876accf51e54c2ae95e08c43936536f0
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2023-04-28 00:20:46 -0700

    partial cat

--- last/eos/USER/cat.c
+++ last/eos/USER/cat.c
@@ -1,77 +1,69 @@
-int my_read(int fd, char *read_buf, int nbytes)
-{
-  OFT *oftp = running->fd[fd];
-  char kbuf[BLKSIZE];
-  int count = 0; // number of bytes read
-  int offset = oftp->offset; // byte offset in file to READ
-  int avil = oftp->minodePtr->INODE.i_size - offset; // compute bytes available in file
-  
-  // 2) Compute start byte in block
-  while (nbytes && avil)
-  {
-    int lbk = offset / BLKSIZE; // compute logical block
-    int start = offset % BLKSIZE; // start byte in block
-    
-    // 3) Convert logical block number to physical block number
-    int blk = read_map(oftp->minodePtr->INODE, lbk);
-    
-    //int blk = oftp->minodePtr->INODE.i_block[lbk];
-    
-    // 4) Read blk into char kbuf[BLKSIZE]
-    get_block(dev, blk, kbuf);
-    char *cp = kbuf + start;
-    int remain = BLKSIZE - start;
-    int use_remain = remain;
-
-    if (nbytes - remain < 0)
-    {
-      use_remain = nbytes;
-    }
-    else if (avil - remain < 0)
-    {
-      use_remain = avil;
-    }
-
-    memcpy(read_buf, cp, use_remain);
-    offset += use_remain; 
-    count += use_remain;
-    avil -= use_remain;
-    nbytes -= use_remain;
-
-    // // // 5) Copy bytes from kbuf[] to buf[]
-    // while (remain)
-    // {
-    //   *read_buf++ = *cp++;
-    //   offset++; count++; // inc offset, count;
-    //   remain--; avil--; nbytes--; // decrease remain, available nbytes
-    //   if (nbytes == 0 || avil == 0) 
-    //   {
-    //     break;
-    //   } 
-    // } // end of while(remain)
-  } // end of while(nbytes && avil)
-  oftp->offset = offset;
-  return count;
-}
+#include "ucode.c"
 
 int cat(char *filename)
 {
   char mybuf[1024];
   int n;
 
-  int fd = my_open(filename, 0);
-  while (n = my_read(fd, mybuf, 1024))
+  int fd = open(filename, 0);
+  while (n = read(fd, mybuf, 1024))
   {
     mybuf[n] = 0; // as a null terminated string
-    printf("%s", mybuf); // this works but not great
-    // handle \n properly
-
+    printf("%s", mybuf);
   }
-  //putchar('\n');
-  my_close(fd);
+
+  close(fd);
 }
 
 main()
 {
-    
+    char buf[1024];
+    char tmp[128];
+    int n;
+
+    struct stat mystat, st_tty, st0, st1;
+    char tty_buf[128];
+
+
+    gettty(tty_buf);
+
+    stat(tty_buf, &st_tty);
+    fstat(0, &st0);
+    fstat(1, &st1);
+
+    if (argc < 2)
+    {
+        if (st_tty.st_ino != st0.st_ino) // stdin has not been redirected
+        {
+            while (n = getline(buf))
+            {
+                buf[n] = 0; // as a null terminated string
+                printf("%s", buf);
+            }
+        }
+        else
+        {
+            while (n = gets(buf))
+            {
+                buf[n] = 0; // as a null terminated string
+                printf("%s", buf);
+            }
+
+        }
+ 
+
+    }
+    else
+    {
+        if (st_tty.st_ino != st1.st_ino) // stdout has been redirected
+        {
+            
+        }
+        else
+        {
+            cat(argv[1]);
+        }
+    }
+
+
 }
